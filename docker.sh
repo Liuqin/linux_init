46e5b3dc0bfedc9d688fd6b9a981550dabbf9e9b
Chalry Liu <287428729@qq.com>
2021-05-21 19:35:48 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,14 +1,14 @@
 
 #!bash/bin
 echo 'install the docker software...'
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update -y
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
