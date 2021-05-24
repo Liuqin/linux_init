ff452504e4fc05d6fe7cdc6f733365f89689cb21
Chalry Liu <287428729@qq.com>
2021-05-24 12:26:56 +0800

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,26 @@
 #! bash/bin
+
+$port= 9999
+while getopts p: opt;
+do
+case $opt in
+p)
+  port=$opt
+   ;;
+esac
+done
+
 sudo docker pull jenkinsci/blueocean
-sudo  mkdir /var/jenkins_home/
-sudo mkdir /jenkins/password/
+$jenkins_home= /var/jenkins_home/
+$jenkins_password= /jenkins/password/
+
+if [ ! -d "$jenkins_home"]; then  
+    sudo mkdir "$jenkins_home"  
+fi  
+if [ ! -d "$jenkins_password"]; then  
+    sudo mkdir "$jenkins_password"  
+fi 
 chown 1000:1000 /var/jenkins_homme
-　
 #　docker容器中jenkins用户和用户组id为1000，需要修改后目录才能映射成功
-docker run -d -p 9999:8080 -v /var/jenkins_home/:/var/jenkins_home/ -v /jenkins/password/:var/jenkins_home/secrets/initialAdminPassword --name="jenkins" jenkinsci/blueocean
+docker run -d -p $port:8080 -v $jenkins_home:/var/jenkins_home/ -v $jenkins_password:var/jenkins_home/secrets/initialAdminPassword --name="jenkins" jenkinsci/blueocean
 
