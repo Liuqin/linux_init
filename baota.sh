5b34d47616b6eb621d32f7d13cac58345f9044f1
liuqin <287428729@qq.com>
2021-05-25 09:47:27 +0800

    update baota

--- baota.sh
+++ baota.sh
@@ -1,6 +1,7 @@
 #! bin/bash
 
 echo ' install the tools of baota pannel in docker'
+docker stop baota123
 docker rm baota123
 docker run -i -t -d --name baota123 -p 20:20 -p 21:21 -p 80:80 -p 443:443 -p 888:888 -p 8888:8888  --dns 8.8.8.8 --dns 8.8.4.4 --privileged=true -v /home/www:/www centos
 docker exec  -it --user root $(docker ps -q -a --filter name=^/baota123$)  /bin/bash
